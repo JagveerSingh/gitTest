6d37c49eb41c26397a95f92547d169975a1b6cac
Jagveer Singh <jagveer@jagveer.singh>
2020-09-07 19:34:01 +0530

    jagveer

---
+++ src/test/java/testing/erureka/server/TestingErurekaSereverApplicationTests.java
@@ -0,0 +1,13 @@
+package testing.erureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingErurekaSereverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
